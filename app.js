bcc9fd23039d7b11071d6073a17ad4355c3f2443
Ashkaan Jaberi <ashkaan14@gmail.com>
2017-05-31 20:54:10 -0700

    Question 6 Added

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 'use strict';
-
+/*
 var correct = 0;
 var wrong = 0;
 
-// Making the Quiz and Answer Key
+// Making the Quiz and Answer Key for Questions 1-5
 var myQuizz = [
   ['Are Fantasy books my favorite kind of books to read?', 'yes', 'y', 'That is correct! Nothing like a good fantasy book to get lost in!'],
   ['Are Horror movies my favorite kind of movies?', 'no', 'n', 'That is correct! I am terrified by horror movies!'],
@@ -32,7 +32,37 @@ for (var i = 0; i < myQuizz.length; i++) {
     console.log(myQuizz[i]);
   }
 }
-
+*/
+//Question 6 | The Number Guessing Question
+var round = 0;
+var question6 = false;
+var myNumber = Math.floor((Math.random() * 20) + 1);
+console.log(myNumber);
+var guess = parseInt(prompt('Guess a number between 1 and 20 - I will give you 4 chances.'));
+while(round < 4){
+  if(guess === myNumber){
+    alert('You guessed it correctly!');
+    question6 = true;
+    correct++;
+    break;
+  }
+  else if(guess > myNumber){
+    alert('Your guess was higher than my number. Try again!');
+    round++;
+    var guess = parseInt(prompt('Guess another number between 1 and 20'));
+    continue;
+  } else {
+    alert('Your guess was lower than my number. Try again!');
+    round++;
+    var guess = parseInt(prompt('Guess another number between 1 and 20'));
+    continue;
+  }
+}
+//Telling the user the results
+if(question6 === false) {
+  alert('You were 0 for 4. Better luck next time pal!');
+}
+/*
 alert('The quiz is over ' + userName + '! Let\'s see the results!');
 
 //Quiz Results Printed
@@ -41,3 +71,4 @@ document.write('<h3>You got ' + correct + '/5 correct!</h3>');
 for (var i = 0; i < myQuizz.length; i++) {
   document.write('<h4>Question 1 asked "' + myQuizz[i][0] + '". You guessed "' + myQuizz[i][4] + '" and the answer to that is "' + myQuizz[i][1] + '"! </h4>');
 };
+*/
