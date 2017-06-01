0b7f677fce44b087a51ad6fcff070a5695930ad9
Ashkaan Jaberi <ashkaan14@gmail.com>
2017-05-31 19:56:08 -0700

    All da arrays

--- app.js
+++ app.js
@@ -11,77 +11,90 @@ var myQuizz = [
   ['Am I a certified scuba diver?', 'yes', 'y']
 ];
 
-for (var i = 0; i < myQuizz.length; i++) {
-  console.log(myQuizz[i][0]);
-}
-
-/*
 var userName = prompt('Hello my name is Ashkaan. What is your name?');
 alert('Hello ' + userName + '! Here is a quiz about me! Please answer Y/N or Yes/No to each question! Here we go!');
 
-var guess1 = prompt(question1);
-if (guess1.toLowerCase() === answer1 || guess1.toLowerCase() === answerOne) {
+var guess1 = prompt(myQuizz[0][0]);
+if (guess1.toLowerCase() === myQuizz[0][1] || guess1.toLowerCase() === myQuizz[0][2]) {
   alert('That is correct! Nothing like a good fantasy book to get lost in!');
   correct ++;
   console.log('correct= ' + correct);
+  myQuizz[0][3] = guess1;
+  console.log(myQuizz[0]);
 } else {
   alert('That is incorrect! Better luck next question :)');
   wrong ++;
   console.log('wrong= ' + wrong);
+  myQuizz[0][3] = guess1;
+  console.log(myQuizz[0]);
 }
 
-var guess2 = prompt(question2);
-if (guess2.toLowerCase() === answer2 || guess2.toLowerCase() === answerTwo) {
+var guess2 = prompt(myQuizz[1][0]);
+if (guess2.toLowerCase() === myQuizz[1][1] || guess2.toLowerCase() === myQuizz[1][2]) {
   alert('That is correct! I am terrified by horror movies!');
   correct ++;
   console.log('correct= ' + correct);
+  myQuizz[1][3] = guess2;
+  console.log(myQuizz[1]);
 } else {
   alert('That is incorrect! Better luck next question :)');
   wrong ++;
   console.log('wrong= ' + wrong);
+  myQuizz[1][3] = guess2;
+  console.log(myQuizz[1]);
 }
 
-var guess3 = prompt(question3);
-if (guess3.toLowerCase() === answer3 || guess3.toLowerCase() === answerThree) {
+var guess3 = prompt(myQuizz[2][0]);
+if (guess3.toLowerCase() === myQuizz[2][1] || guess3.toLowerCase() === myQuizz[2][2]) {
   alert('That is correct! I was in chess club growing up!');
   correct ++;
   console.log('correct= ' + correct);
+  myQuizz[2][3] = guess3;
+  console.log(myQuizz[2]);
 } else {
   alert('That is incorrect! Better luck next question :)');
   wrong ++;
   console.log('wrong= ' + wrong);
+  myQuizz[2][3] = guess3;
+  console.log(myQuizz[2]);
 }
 
-var guess4 = prompt(question4);
-if (guess4.toLowerCase() === answer4 || guess4.toLowerCase() === answerFour) {
+var guess4 = prompt(myQuizz[3][0]);
+if (guess4.toLowerCase() === myQuizz[3][1] || guess4.toLowerCase() === myQuizz[3][2]) {
   alert('That is correct! I would drown 10/10 times if I tried to play water polo!');
   correct ++;
   console.log('correct= ' + correct);
+  myQuizz[3][3] = guess4;
+  console.log(myQuizz[3]);
 } else {
   alert('That is incorrect! Better luck next question :)');
   wrong ++;
   console.log('wrong= ' + wrong);
+  myQuizz[3][3] = guess4;
+  console.log(myQuizz[3]);
 }
 
-var guess5 = prompt(question5);
-if (guess5.toLowerCase() === answer5 || guess5.toLowerCase() === answerFive) {
+var guess5 = prompt(myQuizz[4][0]);
+if (guess5.toLowerCase() === myQuizz[4][1] || guess5.toLowerCase() === myQuizz[4][2]) {
   alert('That is correct! My first dive ever I saw a shark!');
   correct ++;
   console.log('correct= ' + correct);
+  myQuizz[4][3] = guess5;
+  console.log(myQuizz[4]);
 } else {
   alert('That is incorrect! Better luck next question :)');
   wrong ++;
   console.log('wrong= ' + wrong);
+  myQuizz[4][3] = guess5;
+  console.log(myQuizz[4]);
 }
 
 alert('The quiz is over ' + userName + '! Let\'s see the results!');
 
 document.write('<h2>Here are the results to the quiz you took about me ' + userName + '!</h2>');
 document.write('<h3>You got ' + correct + '/5 correct!</h3>');
-document.write('<h4>Question 1 asked "' + question1 + '". You guessed "' + guess1.toLowerCase() + '" and the answer to that is "yes"! </h4>');
-document.write('<h4>Question 2 asked "' + question2 + '". You guessed "' + guess2.toLowerCase() + '" and the answer to that is "no"! </h4>');
-document.write('<h4>Question 3 asked "' + question3 + '". You guessed "' + guess3.toLowerCase() + '" and the answer to that is "yes"! </h4>');
-document.write('<h4>Question 4 asked "' + question4 + '". You guessed "' + guess4.toLowerCase() + '" and the answer to that is "no"! </h4>');
-document.write('<h4>Question 5 asked "' + question5 + '". You guessed "' + guess5.toLowerCase() + '" and the answer to that is "yes"! </h4>');
-
-*/
+document.write('<h4>Question 1 asked "' + myQuizz[0][0] + '". You guessed "' + myQuizz[0][3] + '" and the answer to that is "' + myQuizz[0][1] + '"! </h4>');
+document.write('<h4>Question 2 asked "' + myQuizz[1][0] + '". You guessed "' + myQuizz[1][3] + '" and the answer to that is "' + myQuizz[1][1] + '"! </h4>');
+document.write('<h4>Question 3 asked "' + myQuizz[2][0] + '". You guessed "' + myQuizz[2][3] + '" and the answer to that is "' + myQuizz[2][1] + '"! </h4>');
+document.write('<h4>Question 4 asked "' + myQuizz[3][0] + '". You guessed "' + myQuizz[3][3] + '" and the answer to that is "' + myQuizz[3][1] + '"! </h4>');
+document.write('<h4>Question 5 asked "' + myQuizz[4][0] + '". You guessed "' + myQuizz[4][3] + '" and the answer to that is "' + myQuizz[4][1] + '"! </h4>');
