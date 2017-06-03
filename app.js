771eab09a71a9a23de1f0048abf0cb147e3be5c2
Ashkaan Jaberi <ashkaan14@gmail.com>
2017-06-03 15:19:01 -0700

    pair programming time

--- app.js
+++ app.js
@@ -17,9 +17,10 @@ var myQuizz = [
 //Listing the answers to question 7 to make things easier later
 var questionSevenAnswers = [];
 for(var i = 1; i < 5; i++){
-  questionSevenAnswers.push(' ' + myQuizz[6][i]);
+  questionSevenAnswers.push(myQuizz[6][i]);
 }
 
+/*
 alert('SURPRISE! POP QUIZ!');
 var userName = prompt('Let\'s start off with an easy one. First, what is your name?');
 alert('Hello ' + userName + '! Good luck in ths Pop Quiz! Please answer Y/N or Yes/No to questions 1-5! Here we go!');
@@ -48,51 +49,34 @@ var question6 = false;
 var myNumber = Math.floor((Math.random() * 20) + 1);
 console.log(myNumber);
 var guess6 = parseInt(prompt('Guess a number between 1 and 20 - I will give you 4 chances.'));
-while(round < 4){
-  if(guess6 === myNumber){
+while(round <= 4){
+  if(round === 4){
+    alert('You were 0 for 4. Better luck next time pal!');
+    break;
+  } else if(guess6 === myNumber){
     alert('You guessed it correctly!');
     question6 = true;
     correct++;
     break;
   }
   else if(guess6 > myNumber){
-    alert('Your guess was higher than my number. Try again!');
+    var guess6 = parseInt(prompt('Your guess was too high. Try again.'));
     round++;
-    var guess6 = parseInt(prompt('Guess another number between 1 and 20'));
     continue;
   } else {
-    alert('Your guess was lower than my number. Try again!');
+    var guess6 = parseInt(prompt('Your guess was too low. Try again.'));
     round++;
-    var guess6 = parseInt(prompt('Guess another number between 1 and 20'));
     continue;
   }
-}
-//Telling the user the results
-if(question6 === false) {
-  alert('You were 0 for 4. Better luck next time pal!');
-}
+};
 
+*/
 //Question 7
 var turns = 1;
 var question7 = false;
 var guess7 = prompt(myQuizz[6][0]);
-while(turns < 6){
-  if(guess7.toLowerCase() === myQuizz[6][1] || guess7.toLowerCase() === myQuizz[6][2] || guess7.toLowerCase() === myQuizz[6][3] || guess7.toLowerCase() === myQuizz[6][4]){
-    alert(myQuizz[6][5] + 'I have seen the following while diving: ' + questionSevenAnswers + '!');
-    question7 = true;
-    console.log('correct= ' + correct);
-    break;
-  } else {
-    var guess7 = prompt('Nope haven\'t seen one of those yet. Guess again!');
-    turns++;
-    console.log('this is turn ' + turns);
-    continue;
-  }
-}
-if(question7 === false) {
-  alert('You were 0 for 6. I have seen the following while diving: ' + questionSevenAnswers + '!');
-}
 
+/*
 alert('The quiz is over ' + userName + '! Let\'s see the results!');
 
 //Quiz Results Printed
@@ -114,3 +98,4 @@ if(question7 === true) {
 } else {
   document.write('<h4>You got question 7 wrong :( Here are the correct answers again: ' + questionSevenAnswers + '. </h4>');
 }
+*/
